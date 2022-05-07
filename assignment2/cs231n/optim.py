d603a9dc6980804188a773d8e8daf5855443d138
ZW Mizuno <104285550+ZWMizuno@users.noreply.github.com>
2022-05-07 19:47:14 +0800

    Add files via upload

--- assignment2/cs231n/optim.py
+++ assignment2/cs231n/optim.py
@@ -68,7 +68,9 @@ def sgd_momentum(w, dw, config=None):
     # the next_w variable. You should also use and update the velocity v.     #
     ###########################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-
+    v = config['momentum'] * v - config['learning_rate'] * dw
+    w += v
+    next_w = w
     pass
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
@@ -106,7 +108,9 @@ def rmsprop(w, dw, config=None):
     # config['cache'].                                                        #
     ###########################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-
+    config['cache'] = config['decay_rate'] * config['cache'] + (1 - config['decay_rate']) * dw**2
+    w += -config['learning_rate'] * dw / (np.sqrt(config['cache']) + config['epsilon'])
+    next_w = w
     pass
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
@@ -151,6 +155,13 @@ def adam(w, dw, config=None):
     # using it in any calculations.                                           #
     ###########################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+    config['t'] += 1
+    config['m'] = config['beta1'] * config['m'] + (1 - config['beta1']) * dw
+    mt = config['m'] / (1 - config['beta1']**config['t'])
+    config['v'] = config['beta2'] * config['v'] + (1 - config['beta2']) * (dw**2)
+    vt = config['v'] / (1 - config['beta2']**config['t'])
+    w += -config['learning_rate'] * mt / (np.sqrt(vt) + config['epsilon'])
+    next_w = w
 
     pass
 
